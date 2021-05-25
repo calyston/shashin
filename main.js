be84baccdd5c498cd10bd9ab5eb6d203e23f7c88
Christine <calyston.14@gmail.com>
2021-05-25 11:13:17 +0100

    feat: async function for curated photos created

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const submitButton = document.querySelector('.submit-button');
 let searchValue;
+
+//Functions
+async function curatedPhotos() {
+  const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=1", {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: auth
+    }
+  });
+  const data = await dataFetch.json();
+  console.log(data);
+}
+
+curatedPhotos();
