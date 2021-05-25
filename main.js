58df3a36b033b711904b905bd171830403b61bb8
Christine <calyston.14@gmail.com>
2021-05-25 11:05:54 +0100

    feat: query selectors created

---
+++ main.js
@@ -0,0 +1,8 @@
+//API Key
+const auth = "563492ad6f917000010000014b31429f64334d2aa14a12ec09ec7a2d"
+
+//Query Selectors
+const gallery = document.querySelector('.gallery');
+const searchInput = document.querySelector('.search-input');
+const submitButton = document.querySelector('.submit-button');
+let searchValue;
